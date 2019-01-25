e3fbc0c3000ff22b3d704dd3e1d3688489d6091d
James Hawley <jamesrichardhawley@gmail.com>
2019-01-25 13:58:05 -0500

    Initial conversion to python package
    Adding setup, moving script to __init__

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bed-jaccard',
+      version='0.1',
+      description='Quickly calculate and plot Jaccard index for multiple BED files',
+      url='http://github.com/jrhawley/bed-jaccard',
+      author='James Hawley',
+      author_email='james.hawley@mail.utoronto.ca',
+      license='GPLv3',
+      packages=['bed-jaccard'],
+      zip_safe=True)
