bddce924b020ded27e6fb5392c633c0ee68d7544
James Hawley <jamesrichardhawley@gmail.com>
2019-01-27 12:19:05 -0500

    Better pypi documentation

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
+from os import path
+
+this_dir = path.abspath(path.dirname(__file__))
+with open(path.join(this_dir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='bed-jaccard',
-    version='0.1.1',
+    version='0.1.2',
     description='Quickly calculate and plot Jaccard index for BED files',
     url='http://github.com/jrhawley/bed-jaccard',
     author='James Hawley',
@@ -19,4 +24,7 @@ setup(
         'console_scripts': ['bed-jaccard=bed_jaccard.command_line:main']
     },
     scripts=['bin/bed-jaccard'],
-    zip_safe=True)
+    zip_safe=True,
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
