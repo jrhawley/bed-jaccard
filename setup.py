eafc27f5f975b97fe0c6d87263088903c91a642b
James Hawley <jamesrichardhawley@gmail.com>
2019-01-27 01:18:18 -0500

    v0.1.1 update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bed-jaccard',
-    version='0.1',
+    version='0.1.1',
     description='Quickly calculate and plot Jaccard index for BED files',
     url='http://github.com/jrhawley/bed-jaccard',
     author='James Hawley',
