7b74f60e521240e01065a966c8070e3627f4fdd6
James Hawley <jamesrichardhawley@gmail.com>
2019-02-18 22:17:58 -0500

    Trying to diagnose osx conda issues

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,10 +3,15 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     # download conda
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -o miniconda.sh
+    
+    # diagnose conda issues
+    wc -l miniconda.sh
+    head -n 100 miniconda.sh
+    tail -n 100 miniconda.sh
 
     # install conda
     chmod +x miniconda.sh
-    bash miniconda.sh -b -p $HOME/miniconda3
+    sh miniconda.sh -b -p $HOME/miniconda3
     export PATH="$HOME/miniconda3/bin:$PATH"
 
     # update and configure conda
